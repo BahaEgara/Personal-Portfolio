92cf23af20fc80c109da5a6c5b323f6c391b4547
BahaEgara <bahati.egara@strathmore.edu>
2024-10-21 11:35:55 +0300

    Did the preload animationas it loads

---
+++ script.js
@@ -0,0 +1,27 @@
+function startLoader(){
+    let counterElement = document.querySelector(".counter");
+    let currentValue = 0;
+
+    function updateCounter(){
+        if(currentValue === 100){
+            return;
+        }
+        currentValue += Math.floor(Math.random() * 10) + 1;
+        if (currentValue > 100){
+            currentValue = 100;
+        }
+
+        counterElement.textContent = currentValue;
+
+        let delay =Math.floor(Math.random()*200) + 50;
+        setTimeout(updateCounter.delay);
+    }
+
+    updateCounter();
+
+}
+startLoader();
+WebGLSampler.toString(".counter",0.25 , {
+    delay: 3.5,
+    opacity: 0
+})
